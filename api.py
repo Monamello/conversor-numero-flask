96f25ea2fe5ec1406a63e68863aa028e6b4f7378
Monamello <monacacheada@gmail.com>
2019-02-24 23:54:58 -0300

    Criando projeto em flask para fazer o conversor

---
+++ api.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+import conversor
+app = Flask(__name__)
+
+app.config['JSON_AS_ASCII'] = False
+
+@app.route("/extensao/<string:num>")
+def hello(num):
+    num = int(num)
+    return jsonify({'extenso': conversor.get_extenso(num)})
